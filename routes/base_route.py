be5c575fb7561e6abc3a89a9280b9d130521faea
Husam <husamahmed616@gmail.com>
2026-03-04 21:57:30 +0300

    Base route

---
+++ routes/base_route.py
@@ -0,0 +1,20 @@
+import os
+
+from fastapi import FastAPI, APIRouter, Depends
+from utils import Settings, get_settings
+
+base_router = APIRouter(
+    prefix="/api/v1", 
+    tags=["api_v1"]
+)
+
+@base_router.get("/")
+async def welcome(app_setings:Settings = Depends(get_settings)):
+    app_setings = get_settings()
+    app_name = app_setings.APP_NAME
+    app_version = app_setings.APP_VERSION
+
+    return {
+        "app_name": app_name,
+        "app_version": app_version
+    }
