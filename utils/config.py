4114431c48aec9b51f5c046d14ea0954d863522d
Husam <husamahmed616@gmail.com>
2026-03-04 22:12:44 +0300

    add config parameters

--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
 
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_CHUNCK_SIZE: int
 
     class Config:
         env_file = ".env"
