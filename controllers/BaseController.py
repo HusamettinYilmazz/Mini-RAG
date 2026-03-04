cde5cce595a2c25f00fe092b69e8d333435628ef
Husam <husamahmed616@gmail.com>
2026-03-04 22:10:13 +0300

    Update BaseController

--- controllers/BaseController.py
+++ controllers/BaseController.py
@@ -5,5 +5,6 @@ from utils import Settings, get_settings
 class BaseController:
     def __init__(self):
         self.app_settings: Settings = get_settings()
-        
+        self.base_path = os.path.dirname(__file__)
+        self.root_path = os.path.dirname(self.base_path)
 
