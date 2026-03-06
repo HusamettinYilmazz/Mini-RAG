0521056498558c3a852e0b36f00a1c2f745abb53
Husam <husamahmed616@gmail.com>
2026-03-06 16:59:53 +0300

    Edit Response requests enum at models/enums/ResponseEnum.py

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -5,3 +5,5 @@ class ResponseSignal(Enum):
     FILE_SIZE_ERROR= "file_size_exceeded"
     FILE_UPLOAD_SUCCESS= "file_upload_success"
     FILE_UPLOAD_FAILED= "file_upload_failed"
+    FILE_PROCESSING_SUCCES= "file_processing_succes"
+    FILE_PROCESSING_FAILED= "file_processing_failed"
