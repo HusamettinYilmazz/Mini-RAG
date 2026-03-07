ce644871f6cf4577338ca90f31e58b3ca686e72c
Husam <husamahmed616@gmail.com>
2026-03-07 14:20:26 +0300

    Changing deprecated FastAPI().on_event to asynccontextmanager's lifespan

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
+
 from routes import base_router, data_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from utils.config import get_settings
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
 
     app.mongo_con = AsyncIOMotorClient(settings.DB_MONGO_URL)
     app.db_client = app.mongo_con[settings.DB_MONGO_NAME]
 
-app.on_event("shutdown")
-async def shutdown_db_client():
+    yield  ## application runs
+
     app.mongo_con.close()
 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base_router)
 app.include_router(data_router)
