da66be41359f07d2f036c374f75f021d5605bc8d
Husam <husamahmed616@gmail.com>
2026-03-12 18:32:48 +0300

    Use LLMFactory

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 from routes import base_router, data_router
 from motor.motor_asyncio import AsyncIOMotorClient
 from utils.config import get_settings
+from llm.LLMProviderFactory import LLMProviderFactory
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,6 +12,17 @@ async def lifespan(app: FastAPI):
 
     app.mongo_con = AsyncIOMotorClient(settings.DB_MONGO_URL)
     app.db_client = app.mongo_con[settings.DB_MONGO_NAME]
+
+    llm_provider_factory = LLMProviderFactory(settings)
+    app.emb_client = llm_provider_factory.create(settings.EMBEDDING_BACKEND)
+    app.emb_client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID,
+        emb_size= settings.EMBEDDING_MODEL_SIZE)
+    
+    app.gen_client = llm_provider_factory.create(settings.GENERATION_BACKEND)
+    app.gen_client.set_generation_model(
+        model_id= settings.GENERATION_MODEL_ID
+    )
 
     yield  ## application runs
 
