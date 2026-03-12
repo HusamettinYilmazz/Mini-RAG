e62f9c7c48fd33216ae3aae98f0510083036fedc
Husam <husamahmed616@gmail.com>
2026-03-12 18:09:17 +0300

    Update .env.example

--- src/utils/config.py
+++ src/utils/config.py
@@ -12,6 +12,20 @@ class Settings(BaseSettings):
     DB_MONGO_URL: str
     DB_MONGO_NAME: str
 
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
+
     class Config:
         env_file = ".env"
 
