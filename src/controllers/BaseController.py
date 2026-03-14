579876d6e836a81b33f9bba09c07038c96ff66b5
Husam <husamahmed616@gmail.com>
2026-03-14 16:22:21 +0300

    add get_database_dir

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -8,3 +8,10 @@ class BaseController:
         self.base_path = os.path.dirname(__file__)
         self.root_path = os.path.dirname(self.base_path)
 
+        self.database_dir = os.path.join(self.root_path, "assets/database")
+    
+    def get_database_dir(self, db_name: str):
+        database_path = os.path.join(self.database_dir, db_name)
+        os.makedirs(database_path, exist_ok=True)
+
+        return database_path
