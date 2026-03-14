9c9a3a1751c0159775e21005a9e2cff1ed1e1cac
Husam <husamahmed616@gmail.com>
2026-03-14 16:21:12 +0300

    Update environment config

--- src/utils/config.py
+++ src/utils/config.py
@@ -26,6 +26,10 @@ class Settings(BaseSettings):
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND:str
+    VECTOR_DB_NAME:str
+    VECTOR_DB_DISTANCE_METHOD:str
+
     class Config:
         env_file = ".env"
 
