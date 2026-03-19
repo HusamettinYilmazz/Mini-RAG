069c894b3226f897c23cd0c65a06de1ae6a1375e
Husam <husamahmed616@gmail.com>
2026-03-20 00:20:14 +0300

    Update .env.exmaple

--- src/utils/config.py
+++ src/utils/config.py
@@ -30,6 +30,9 @@ class Settings(BaseSettings):
     VECTOR_DB_NAME:str
     VECTOR_DB_DISTANCE_METHOD:str
 
+    PRIMARY_LANG: str
+    DEFAULT_LANG: str
+
     class Config:
         env_file = ".env"
 
